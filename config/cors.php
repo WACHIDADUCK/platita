88f85f3b4c93880bfa07415594b332275bf233bc
wachipato <116815367+WACHIDADUCK@users.noreply.github.com>
2025-02-17 13:58:43 +0000

    cambiando fonciguracion cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://platita-front-git-main-wachidaducks-projects.vercel.app/'],
+    'allowed_origins' => ['http://localhost:5173', 'https://guillermo.informaticamajada.es'],
 
     'allowed_origins_patterns' => [],
 
