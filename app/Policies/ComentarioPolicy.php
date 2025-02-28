cfaaadaf97adf5338a242d322786d06623a36414
Fabio Acosta <Fabio@Acosta.com>
2025-02-28 22:22:40 +0000

    Policies de Comentarios establecidas

--- app/Policies/ComentarioPolicy.php
+++ app/Policies/ComentarioPolicy.php
@@ -13,7 +13,7 @@ class ComentarioPolicy
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class ComentarioPolicy
      */
     public function view(User $user, Comentario $comentario): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -29,7 +29,7 @@ class ComentarioPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return true;
     }
 
     /**
